a5ea94d1555a6338972b4544933f2fd34ce192be
Akihide Nano <an74abc@gmail.com>
2018-05-17 09:55:05 +0900

    study rust

---
+++ hello_world/target/debug/deps/hello_world-15cbf695c92508b0.d
@@ -0,0 +1,5 @@
+C:\Users\user19\Documents\rust\rust-study\hello_world\target\debug\deps\hello_world-15cbf695c92508b0.exe: src\main.rs
+
+C:\Users\user19\Documents\rust\rust-study\hello_world\target\debug\deps\hello_world-15cbf695c92508b0.d: src\main.rs
+
+src\main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+C:\Users\user19\Documents\rust\rust-study\hello_world\target\debug\hello_world.pdb: C:\Users\user19\Documents\rust\rust-study\hello_world\src\main.rs
